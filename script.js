87b75e05884261084fcdbe3799360a7ec4104fa9
gitVinayakD <49040439+gitVinayakD@users.noreply.github.com>
2023-09-26 15:01:20 -0700

    Added resize event listener and logic.

--- script.js
+++ script.js
@@ -2,24 +2,48 @@ let rowCount = 16;
 
 
 const gameSpace = document.getElementById("gameSpace");
+//function for adding game rows and columns
 function createGameArea(rows) {
-    if (gameSpace.childElementCount==0){
-        for (let i=0; i<rows; i++){
-            const divRow = document.createElement('div');
-            divRow.className="row"+i;
-            divRow.style.setProperty('display','flex');
-            divRow.style.setProperty('flex', 1);
-            gameSpace.appendChild(divRow);
-            for (let j=0; j<rows; j++){
-                const divColumn = document.createElement('div');
-                divColumn.className="column"+j;
-                divColumn.style.setProperty('border-style','solid');
-                divColumn.style.setProperty('flex', 1);
-                divRow.appendChild(divColumn);
-            }
+    while (gameSpace.firstChild) {
+        gameSpace.removeChild(gameSpace.firstChild);
+    }
+    if (rows>100){rows=100;}
+    for (let i=0; i<rows; i++){
+        const divRow = document.createElement('div');
+        divRow.className="row"+i;
+        divRow.style.setProperty('display','flex');
+        divRow.style.setProperty('flex', 1);
+        gameSpace.appendChild(divRow);
+        for (let j=0; j<rows; j++){
+            const divColumn = document.createElement('div');
+            divColumn.className="column"+j;
+            divColumn.style.setProperty('border-style','solid');
+            divColumn.style.setProperty('flex', 1);
+            divRow.appendChild(divColumn);
         }
     }
 }
 
 createGameArea(rowCount);
-console.log(gameSpace);
+
+//creates buttons for Shake and Set Size
+const gameButtons = document.getElementById("gameButtons");
+const shakeButton = document.createElement('button');
+shakeButton.className = "shakeButton";
+shakeButton.textContent = "SHAKE!";
+gameButtons.appendChild(shakeButton);
+const setSizeButton = document.createElement('button');
+setSizeButton.className = "setSizeButton";
+setSizeButton.textContent = "Resize";
+gameButtons.appendChild(setSizeButton);
+
+
+//adds click listener to resize which recreates board based on input size
+setSizeButton.addEventListener('click', () => {
+    createGameArea(parseInt(prompt("How many boxes tall and wide?","16")));
+    }
+);
+
+//add click listener for shake here 
+
+//event listeners for Row/Column divs
